2e6a25d0fbc0fc8e82b471da1edf67a2df4ef5de
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2023-06-05 12:26:30 +0530

    Feat: Add CI/CD to the Github

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	root: true,
+	extends: [
+		'plugin:@wordpress/eslint-plugin/recommended',
+		'plugin:eslint-comments/recommended',
+	],
+	env: {
+		browser: true,
+		jquery: true,
+	},
+	rules: {
+		// Add rules.
+	},
+};
